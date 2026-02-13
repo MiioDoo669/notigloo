9d900f549d381d19ef0a33787d735c5b3c9eab1f
MiioDoo669 <myomyatoo265@gmail.com>
2026-02-13 09:52:30 +0630

    fake

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pygame
 from moviepy.editor import VideoFileClip # Audio extract လုပ်ရန်
 import os
 
-# --- ၁။ Video ထဲက အသံကို Extract လုပ်ခြင်း ---
+
 video_path = "alarm.mp4"
 temp_audio = "temp_audio.mp3"
 
@@ -14,11 +14,11 @@ if not os.path.exists(temp_audio):
     video = VideoFileClip(video_path)
     video.audio.write_audiofile(temp_audio)
 
-# --- ၂။ Pygame Setup ---
+
 pygame.mixer.init()
 pygame.mixer.music.load(temp_audio)
 
-# Camera & Video Setup
+
 cam = cv2.VideoCapture(0)
 cap_v = cv2.VideoCapture(video_path)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
@@ -47,13 +47,13 @@ while True:
             blink_duration = time.time() - blink_start_time
             
             if blink_duration > drowsy_limit:
-                # Video Frame ဖတ်ခြင်း
+                
                 ret, v_frame = cap_v.read()
                 if not ret:
                     cap_v.set(cv2.CAP_PROP_POS_FRAMES, 0)
                     ret, v_frame = cap_v.read()
                 
-                # Window အသစ်ဖြင့် Video ပြခြင်း
+               
                 cv2.imshow('EMERGENCY ALARM VIDEO', v_frame)
                 
                 if not video_playing:
